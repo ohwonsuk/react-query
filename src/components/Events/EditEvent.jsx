e96c0b115b15b3ec0db1a4aeb73d3faa8c25fd84
ohwonsuk <wsoh51@gmail.com>
2024-02-12 19:14:59 +0900

    optimistic update적용

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,20 +1,87 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams();
 
-  function handleSubmit(formData) {}
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
 
-  function handleClose() {
-    navigate('../');
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    // onMutate 속성을 사용시 optimistic update로 쿼리데이터 업데이트 하여
+    // - 백엔드로 수정데이터 업데이트 하는 동안
+    // 캐싱되어 있는 폼데이터를 바로 적용, 만약 백엔드 저장실패시 롤백적용
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+      const previousEvent = queryClient.getQueryData(["events", params.id]);
+
+      queryClient.setQueryData(["events", params.id], newEvent);
+
+      return { previousEvent };
+    },
+    // mutate 실패시 context에는 mutate에서 return된 previousEvent 값이 포함
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", params.id], context.previousEvent);
+    },
+    //onSettled 는 mutate 성공/실패 상관없이 끝날 때 적용되는 속성. 백엔드에서 최신값 적용 (동기화)
+    onSettled: () => {
+      queryClient.invalidateQueries(["events"]);
+    },
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate("../"); // 원래 페이지인 상세페이지 화면으로 이동
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  function handleClose() {
+    navigate("../");
+  }
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div className="center">
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <>
+        <ErrorBlock
+          title="Failed to load event"
+          message={
+            error.info?.message ||
+            "Failed to load event. Please check your inputs and try again later."
+          }
+        />
+        <div className="form-actions">
+          <Link to="../" className="button">
+            Okay
+          </Link>
+        </div>
+      </>
+    );
+  }
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +89,8 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
-    </Modal>
-  );
+    );
+  }
+
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
